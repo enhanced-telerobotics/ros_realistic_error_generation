f4afc756d5208d319c0e5275580d8aedbaa10a94
Shuyuan Yang <sxy841@case.edu>
2025-06-03 17:13:04 -0400

    supoort non ros2 installation

--- realistic_error_generation/utils.py
+++ realistic_error_generation/utils.py
@@ -2,18 +2,6 @@ from pathlib import Path
 from typing import Dict, Tuple
 from kincalib.Learning.NoiseGenerator import NetworkNoiseGenerator
 import json
-from sensor_msgs.msg import JointState
-import numpy as np
-
-
-def msg2numpy(msg: JointState) -> np.ndarray:
-    return np.array(msg.position)
-
-def numpy2msg(arr: np.ndarray) -> JointState:
-    joint_state_msg = JointState()
-
-    joint_state_msg.position = arr.tolist()
-    return JointState(position=arr)
 
 
 def load_noise_generator(
@@ -45,11 +33,7 @@ def load_noise_generator(
 
 
 def get_path_to_torch_model() -> Path:
-    from ament_index_python.packages import get_package_share_directory
-
-    package_share_directory = Path(
-        get_package_share_directory("realistic_error_generation")
-    )
+    package_share_directory = Path(__file__).parent
 
     package_share_directory = (
         package_share_directory / "resources/sample_neural_net_dataset4"
